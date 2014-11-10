0280a40fff0494ecea853c713d7d6d46dc0dc0a3
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2014-11-10 19:26:17 +0200

    added base Maybe code

---
+++ Monads/Maybe.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Monads
+{
+    [DebuggerStepThrough]
+    public struct Maybe
+    {
+        public static Maybe<T> Apply<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+    }
+
+    [DebuggerStepThrough]
+    public struct Maybe<T>
+    {
+        readonly T value;
+        readonly bool isSome;
+
+        public Maybe(T value)
+        {
+            isSome = value != null;
+            this.value = value;
+        }
+
+        public bool IsSome { get { return isSome; } }
+
+        public bool IsNone { get { return !isSome; } }
+
+        public static readonly Maybe<T> None = new Maybe<T>();
+
+        public Maybe<A> Bind<A>(Func<T, A> func)
+        {
+            if (func == null) throw new ArgumentNullException("func");
+
+            return isSome ? Maybe.Apply(func(value)) : Maybe<A>.None;
+        }
+
+        public Maybe<A> Bind<A>(Func<T, Maybe<A>> func)
+        {
+            if (func == null) throw new ArgumentNullException("func");
+
+            return isSome ? func(value) : Maybe<A>.None;
+        }
+
+        public A Match<A>(Func<A> none, Func<T, A> some)
+        {
+            if (none == null) throw new ArgumentNullException("none");
+            if (some == null) throw new ArgumentNullException("some");
+
+            return isSome ? some(value) : none();
+        }
+
+        public void Match(Action none, Action<T> some)
+        {
+            if (none == null) throw new ArgumentNullException("none");
+            if (some == null) throw new ArgumentNullException("some");
+
+            if (isSome) some(value);
+
+            else none();
+        }
+    }
+}
