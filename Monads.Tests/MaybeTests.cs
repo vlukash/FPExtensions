09676459f14a5bb851ae10bcb7d9348e672b1a7c
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2014-11-10 19:28:42 +0200

    added Maybe tests

---
+++ Monads.Tests/MaybeTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+
+namespace Monads.Tests.MonadsTests
+{
+    [TestFixture]
+    public class MaybeTests
+    {
+        [Test]
+        public void ToMaybe_should_not_thrown_exception_on_null_reference()
+        {
+            string nullStr = null;
+            Maybe.Apply(nullStr);
+        }
+
+        [Test]
+        public void ToMaybe_should_not_throw_exception_on_nullable_value_type()
+        {
+            int? i = null;
+            Maybe.Apply(i);
+        }
+
+        [Test]
+        public void IsNone_should_return_true_if_value_is_null()
+        {
+            string nullStr = null;
+            var strMaybe = Maybe.Apply(nullStr);
+            Assert.IsTrue(strMaybe.IsNone);
+        }
+
+        [Test]
+        public void IsNone_should_return_false_if_value_is_not_null()
+        {
+            string str = "not null";
+            var strMaybe = Maybe.Apply(str);
+            Assert.IsFalse(strMaybe.IsNone);
+        }
+
+        [Test]
+        public void IsSome_should_return_false_if_value_is_null()
+        {
+            string nullStr = null;
+            var strMaybe = Maybe.Apply(nullStr);
+            Assert.IsFalse(strMaybe.IsSome);
+        }
+
+        [Test]
+        public void IsSome_should_return_true_if_value_is_not_null()
+        {
+            string str = "not null";
+            var strMaybe = Maybe.Apply(str);
+            Assert.IsTrue(strMaybe.IsSome);
+        }
+
+        [Test]
+        public void Bind_should_return_Mybe_with_not_null_for_not_nullable_result()
+        {
+            string str = "not null";
+            var result = Maybe.Apply(str).Bind(s => s.IndexOf("l"));
+            Assert.IsTrue(result.IsSome);
+        }
+
+        [Test]
+        public void Bind_should_return_Maybe_with_null_for_nullable_result()
+        {
+            string str = "not null";
+            var result = Maybe.Apply(str)
+                              .Bind(s => s.IndexOf("T"))
+                              .Bind(i => i > 0 ? "not null" : null);
+            Assert.IsTrue(result.IsNone);
+        }
+
+        [Test]
+        public void Bind_should_return_Maybe_with_null_for_struct_type()
+        {
+            string str = null;
+            var result = Maybe.Apply(str).Bind(s => s.IndexOf("T"));
+            Assert.IsTrue(result.IsNone);
+        }
+    }
+
+}
